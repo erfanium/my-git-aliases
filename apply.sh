959db5dc9e770210091b7432f54531329cec3fc8
Erfan Safari <erfanshield@outlook.com>
2025-08-12 23:19:17 +0330

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,1 +1,2 @@
-git config --global alias.pretty-branches "!git for-each-ref --sort=-committerdate refs/heads/ --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:relative)%(color:reset) [%(authorname)]'"
+# git recent
+git config --global alias.recent "for-each-ref --sort=-committerdate refs/heads/ --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:relative)%(color:reset) [%(authorname)]'"
