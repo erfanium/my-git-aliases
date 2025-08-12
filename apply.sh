5b95ea4617be00a0e1234ab0c688476a37c9587d
Erfan Safari <erfanshield@outlook.com>
2025-08-12 23:35:54 +0330

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,2 +1,13 @@
 # git recent
-git config --global alias.recent "for-each-ref --sort=-committerdate refs/heads/ --format='%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(committerdate:relative)%(color:reset) [%(authorname)]'"
+git config --global alias.recent "!f() { \
+  current=\$(git symbolic-ref --quiet --short HEAD); \
+  git for-each-ref --sort=-committerdate refs/heads/ \
+    --format='%(refname:short)|%(committerdate:relative)|%(upstream:short)' | \
+  awk -v curr=\"\$current\" -F'|' 'BEGIN {print \"BRANCH | LAST COMMIT | UPSTREAM\"} \
+  { \
+    branch=\$1; \
+    if(branch == curr) branch=\"* \" branch; \
+    printf \"%-30s | %-15s | %s\n\", branch, \$2, \$3; \
+  }' | \
+  column -t -s '|' -o ' | '; \
+}; f"
